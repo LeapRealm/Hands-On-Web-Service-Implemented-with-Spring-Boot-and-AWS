d16d44584dfc01c56cafb881eae3841d08056f01
JinwooKim <jinwookim@leaprealm.com>
2021-09-29 10:50:01 +0900

    Hello Controller 코드를 롬북으로 전환하기

--- src/main/java/com/leaprealm/book/springboot/web/HelloController.java
+++ src/main/java/com/leaprealm/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.leaprealm.book.springboot.web;
 
+import com.leaprealm.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") Integer amount) {
+        return new HelloResponseDto(name, amount);
     }
 }

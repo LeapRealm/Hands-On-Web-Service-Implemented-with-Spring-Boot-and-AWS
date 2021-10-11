75207485019a0d8807ab50e47d464cee1ae8a8f4
JinwooKim <jinwookim@leaprealm.com>
2021-10-11 12:19:35 +0900

    무중단 배포 스크립트 만들기

--- scripts/health.sh
+++ scripts/health.sh
@@ -15,11 +15,11 @@ sleep 10
 for RETRY_COUNT in {1..10}
 do
   RESPONSE=$(curl -s http://localhost:"${IDLE_PORT}"/profile)
-  UP_COUNT=$(echo "${RESPONSE}" | grep 'real' | wc -1)
+  UP_COUNT=$(echo "${RESPONSE}" | grep 'real' | wc -l)
 
   if [ "${UP_COUNT}" -ge 1 ]; then  # ${UP_COUNT} >= 1 ("real" 문자열이 있는지 검증)
       echo "> Health check 성공"
-      switch_proxy
+      switchProxy
       break
   else
       echo "> Health check 의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
